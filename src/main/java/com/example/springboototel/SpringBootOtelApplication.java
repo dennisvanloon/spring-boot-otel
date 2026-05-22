b397464c0998c03f408945551f7652e458c85b6b
Dennis <dennisvanloon.nl@gmail.com>
2026-05-22 22:09:16 +0200

    Initial commit

---
+++ src/main/java/com/example/springboototel/SpringBootOtelApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboototel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOtelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOtelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboototel/SpringBootOtelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboototel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOtelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
